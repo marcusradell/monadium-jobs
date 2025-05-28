8c2964daa65a96092bcf9e180083b7789ab8995c
Marcus Rådell <marcus@radell.net>
2025-05-28 15:51:12 +0200

    show basic info about jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1>Monadium Jobs</h1>;
+"use server";
+
+import { db } from "@/db";
+import { jobs } from "@/db/schema";
+
+export default async function Home() {
+  const jobsData = await db.select().from(jobs).limit(100);
+  const jobsViewModel = jobsData.map(
+    ({ id, headline, webpage_url, application_deadline }) => ({
+      headline,
+      id,
+      webpage_url,
+      application_deadline,
+    }),
+  );
+  return (
+    <>
+      <h1>Monadium Jobs</h1>
+      <pre>{JSON.stringify(jobsViewModel, null, 2)}</pre>
+    </>
+  );
 }
