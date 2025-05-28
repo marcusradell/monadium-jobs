7b850220dae535c43a5c262081c4cab1218df01c
Marcus Rådell <marcus@radell.net>
2025-05-28 12:23:17 +0200

    setup tailwind typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="coffee">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased prose`}
       >
         {children}
       </body>
