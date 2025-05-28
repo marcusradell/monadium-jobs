3ce5179ff6947e186939b6ac0db4ed582f5dcfb0
Marcus Rådell <marcus@radell.net>
2025-05-28 12:33:28 +0200

    set a container class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased prose`}
       >
-        {children}
+        <main className="container mx-auto px-4">{children}</main>
       </body>
     </html>
   );
