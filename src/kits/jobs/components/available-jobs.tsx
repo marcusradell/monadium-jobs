d7444887e92409e099eeef121caab64d8cd5cea8
Marcus Rådell <marcus@radell.net>
2025-06-04 22:57:30 +0200

    filter for software developer jobs

--- src/kits/jobs/components/available-jobs.tsx
+++ src/kits/jobs/components/available-jobs.tsx
@@ -2,11 +2,22 @@
 
 import { db } from "@/db";
 import { jobs } from "@/db/schema";
-import { count } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
+
+const softwareDeveloperConceptId = "DJh5_yyF_hEM";
 
 export async function AvailableJobs() {
-  const jobsData = await db.select().from(jobs).limit(100);
-  const jobsCount = await db.$count(jobs);
+  const jobsData = await db
+    .select()
+    .from(jobs)
+    .where(eq(jobs.occupation_group_concept_id, softwareDeveloperConceptId))
+    .limit(100);
+
+  const [{ count: jobsCount }] = await db
+    .select({ count: sql<number>`count(*)` })
+    .from(jobs)
+    .where(eq(jobs.occupation_group_concept_id, softwareDeveloperConceptId));
+
   const jobsViewModel = jobsData.map(
     ({
       id,
@@ -25,23 +36,26 @@ export async function AvailableJobs() {
     }),
   );
 
-  return jobsViewModel.map((job) => {
-    return (
-      <div key={job.id}>
-        <p>
-          Found <em className="font-bold">{jobsCount} </em> jobs.
-        </p>
-        <h2>{job.headline}</h2>
-        <h3>{job.id}</h3>
-        <a href={job.webpage_url ?? ""}>{job.webpage_url}</a>
-        <p className="wrap-anywhere">
-          <div
-            dangerouslySetInnerHTML={{
-              __html: job.description_text_formatted!,
-            }}
-          />
-        </p>
-      </div>
-    );
-  });
+  return (
+    <>
+      <p>
+        Found <em className="font-bold">{jobsCount} </em> jobs.
+      </p>
+      {jobsViewModel.map((job) => {
+        return (
+          <div key={job.id}>
+            <h2>{job.headline}</h2>
+            <h3>{job.id}</h3>
+            <a href={job.webpage_url ?? ""}>{job.webpage_url}</a>
+            <div
+              className="wrap-anywhere"
+              dangerouslySetInnerHTML={{
+                __html: job.description_text_formatted!,
+              }}
+            />
+          </div>
+        );
+      })}
+    </>
+  );
 }
