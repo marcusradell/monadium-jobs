1351781dc11a1621ed66166a81ae2f4577eb0d2c
Marcus Rådell <marcus@radell.net>
2025-06-04 10:45:25 +0200

    add non-functional jobs search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,35 @@
 
 import { AvailableJobs } from "@/kits/jobs";
 
+function Search() {
+  return (
+    <label className="input">
+      <svg
+        className="h-[1em] opacity-50"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <g
+          strokeLinejoin="round"
+          strokeLinecap="round"
+          strokeWidth="2.5"
+          fill="none"
+          stroke="currentColor"
+        >
+          <circle cx="11" cy="11" r="8"></circle>
+          <path d="m21 21-4.3-4.3"></path>
+        </g>
+      </svg>
+      <input type="search" placeholder="Search jobs..." />
+    </label>
+  );
+}
+
 export default async function Home() {
   return (
     <>
       <h1>Available Jobs</h1>
+      <Search />
       <AvailableJobs />
     </>
   );
